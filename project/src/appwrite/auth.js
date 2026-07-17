4c40cee40f238cd7167f8504475b278677094eae
viralthakkar2005 <viralthakkar1705@gmail.com>
2026-07-17 16:27:41 +0530

    project_appwrite_config

---
+++ project/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from '../conf/conf.js';
+
+import { Client, Account, ID } from "appwrite";
+
+
+//this is class that hold the all logic
+export class AuthService{
+  client=new Client();
+  account;
+
+  constructor(){
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.account=new Account(this.Client);
+  }
+
+  async createAccount({email,password,name}){
+    try {
+      const userAccount =await this.account.create(ID.unique(),email,password,name);
+
+      if(userAccount){
+        // call authore method
+        return this.login({email,password})
+      }
+      else{
+        return userAccount;
+      }
+
+    } catch (error) {
+      throw error; 
+    }
+  }
+
+  async login({email,password}){
+    try {
+      return await this.account.createEmailPasswordSession(email,password);
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async getCurrentUser(){
+    try {
+      await this.account.get();     
+    } catch (error) {
+      throw error
+    }
+
+    return null;
+  }
+
+  async logout(){
+    try {
+      await this.account.deleterSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  
+}
+
+const authService=new AuthService();    // this is object so we use all calass method using like authService.createAccount(pass the value)
+
+export default authService
